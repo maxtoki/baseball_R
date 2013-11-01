6503460f73f1a74109b77a9d2d4724e27f27cde2
Max <profpepper@virgilio.it>
2013-11-01 14:55:33 +0100

    Update _setWorkingDir.R

--- scripts/_setWorkingDir.R
+++ scripts/_setWorkingDir.R
@@ -2,4 +2,4 @@
 # setting the working directory for all the scripts
 ###################################################
 
-setwd("H:/documenti/GitHub/baseball_R/data")
+setwd("H:/documenti/GitHub/baseball_R/data") # modify the "H:/documenti" part to reflect your local GitHub directory
